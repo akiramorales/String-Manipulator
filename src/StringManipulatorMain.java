0ab0c9f3bddc102ffacb25e0f8455396d87795bb
Akira Morales <akira.k.morales@gmail.com>
2018-10-16 13:35:32 -0400

    Add files via upload

---
+++ src/StringManipulatorMain.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class StringManipulatorMain {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Which method would you like to run?");
+		String methodChoice = in.nextLine();
+		System.out.println("Please input a string");
+		String input = in.nextLine();
+
+	}
+
+}
