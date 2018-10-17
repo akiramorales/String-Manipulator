e727169eca8f40a4dfc36716da9645e6026bf1c5
Akira Morales <akira.k.morales@gmail.com>
2018-10-17 13:35:38 -0400

    Add files via upload

--- src/StringManipulator.java
+++ src/StringManipulator.java
@@ -1,4 +1,45 @@
-
 public class StringManipulator {
 
+	public String noVowels(String strIn)
+	{
+		String strReturn = "";
+		for(int i = 0; i < strIn.length(); i++)
+		{
+			if(strIn.charAt(i) == 'a' || strIn.charAt(i) == 'A')
+			{
+				return strReturn +  "*";
+			}
+			else if(strIn.charAt(i) == 'e' || strIn.charAt(i) == 'E')
+			{
+				return strReturn + "*";
+			}
+			else if(strIn.charAt(i) == 'i' || strIn.charAt(i) == 'I')
+			{
+				return strReturn + "*";
+			}
+			else if(strIn.charAt(i) == 'o' || strIn.charAt(i) == 'O')
+			{
+				return strReturn + "*";
+			}
+			else if(strIn.charAt(i) == 'u' || strIn.charAt(i) == 'U')
+			{
+				return strReturn + "*";
+			}
+			else
+			{
+				return strReturn + strIn.charAt(i);
+			}
+		}
+		return strReturn;
+	}
+	
+	public String reverse(String strIn)
+	{
+		String strReturn = "";
+		for(int i = strIn.length()-1; i < strIn.length(); i--)
+		{
+			return strReturn + strIn.charAt(i);
+		}
+		return strReturn;
+	}
 }
--- src/StringManipulatorMain.java
+++ src/StringManipulatorMain.java
@@ -3,11 +3,49 @@ public class StringManipulatorMain {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		boolean playAgain = true;
+		while(playAgain)
+		{
 		System.out.println("Which method would you like to run?");
 		String methodChoice = in.nextLine();
+		while(!methodChoice.equals("noVowels") && methodChoice.equals("reverse"))
+		{
+			System.out.println("Please input \"noVowels\"  or \"reverse\"");
+			methodChoice = in.nextLine();
+		}
+		if (methodChoice.equals("noVowels"))
+		{
 		System.out.println("Please input a string");
-		String input = in.nextLine();
-
+		String str = in.nextLine();
+		String returnString = str.noVowels();
+		System.out.println(returnString);
+		}
+		else if(methodChoice.equals("reverse"))
+		{
+			System.out.println("Please input a string");
+			String str = in.nextLine();
+			String returnString = str.reverse();
+			System.out.println(returnString);
+		}
+		else
+		{
+			System.out.println("Play again?");
+			String answer = in.nextLine();
+			if(!answer.equals("yes")||answer.equals("no"))
+			{
+				System.out.println("Please enter \"yes\" or \"no\"");
+				answer = in.nextLine();
+			}
+			else if (answer.equals("yes"))
+			{
+				playAgain = true;
+			}
+			else if (answer.equals("no"))
+			{
+				playAgain = false;
+			}
+		}
+		}
 	}
 
 }
